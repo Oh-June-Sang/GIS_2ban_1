28322b9af1cd20619d1eb7ca9f839f7b63593c5d
OhJuneSang <mktb5576@naver.com>
2021-07-15 16:45:42 +0900

    7/15 2ban class commit 1

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'accountapp'
 ]
 
@@ -144,4 +145,4 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
-LOGOUT_REDIRECT_URL = reverse_lazy('accouuntapp:login')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
