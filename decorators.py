d18a4cdb6693b26223976b8f1cfade5ccb9eb57c
OhJuneSang <mktb5576@naver.com>
2021-07-21 12:40:44 +0900

    7/21 2ban class commit 4

--- decorators.py
+++ decorators.py
@@ -13,4 +13,23 @@ def hello_world(input_text):
     print(input_text)
     print("함수 끝!")
 
-hello_world("Hello world")
+
+def check_integer(func):
+    def decorated(width, height):
+        if width >=0 and height >=0 :
+            return func(width,height)
+        else:
+            raise ValueError("inpuut must be post")
+    return decorated
+
+
+@check_integer
+def rect_area(width, height):
+    return width * height
+
+@check_integer
+def tri_area(width, height):
+    return 0.5 * width * height
+
+print(tri_area(1,2))
+print(rect_area(1,2))
