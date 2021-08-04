28509e8afcdf0092e78eb5470e2d62dc809e33f7
OhJuneSang <mktb5576@naver.com>
2021-08-04 11:04:25 +0900

    8/4 2ban class commit 1

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -14,5 +17,3 @@ class ArticleCreateView(CreateView):
     def form_valid(self, form):
         form.instance.writer = self.request.user
         return super().form_valid(form)
-
-
